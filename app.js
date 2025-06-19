4434bc2da3ee673b1dcd315a128884bb23f95221
Fvazquez <foapgenerico@365.stucom.com>
2025-06-19 19:56:45 +0200

    stucom

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // DEPENDENCIAS DEL PROYECTO
 const path = require('node:path');
 const fs = require('node:fs');
+const crypto = require('node:crypto');
 const express = require('express'); // Sintaxis commonjs
 const app = express();
 
@@ -14,6 +15,9 @@ app.use(express.static(path.join(__dirname, "public")))
 // Para indicar cuál es el motor de las plantillas
 app.set('view engine', "ejs");
 app.set("views", path.join(__dirname, "views"));
+// Recodificar los datos enviados desde el formulario
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 // CARGAR LOS DATOS
 const jsonData = require('./data/travels.json');
@@ -35,6 +39,22 @@ jsonData.forEach(travel => {
 
 app.get("/admin", (req, res) => {
     res.render("admin", {jsonData});
+})
+
+app.post("/insert", (req, res) => {
+    // console.log(req.body);
+    let newTravel = req.body;
+    if (newTravel[0] != "/") {
+        newTravel.ruta = "/"+newTravel.ruta;
+    }
+    
+    newTravel.precio = parseFloat(newTravel.precio);
+    newTravel.id = crypto.randomUUID();
+    jsonData.push(newTravel);
+    // console.log(jsonData);
+    fs.writeFileSync(path.join(__dirname, "data", "travels.json"), JSON.stringify(jsonData, null, 2), "utf-8");
+    res.redirect("/admin");  
+
 })
 
 
