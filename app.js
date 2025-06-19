c667b6516ca7b65b8e4921f06bc5ca62f71607b1
Fvazquez <foapgenerico@365.stucom.com>
2025-06-19 17:37:51 +0200

    stucom

--- app.js
+++ app.js
@@ -15,11 +15,25 @@ app.use(express.static(path.join(__dirname, "public")))
 app.set('view engine', "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-
-// RUTAS
-app.get('/', (req, res) => {
-    res.render("travels");
+// CARGAR LOS DATOS
+const jsonData = require('./data/travels.json');
+// console.log(jsonData);
+jsonData.forEach(travel => {
+    // RUTAS
+    app.get(`${travel.ruta}`, (req, res) => {
+        res.render("travels", {
+            "lugar" : `${travel.lugar}`,
+            "nombre" : `${travel.nombre}`,
+            "descripcion" : `${travel.descripcion}`,
+            "precio" : `${travel.precio}`,
+            "img" : `${travel.img}`,
+            "id" : `${travel.id}`,
+            "travels" : jsonData
+        });
+    })
 })
+
+
 
 
 app.listen(PORT, () => {console.log(`servidor levantado en http://localhost:${PORT}`);})
